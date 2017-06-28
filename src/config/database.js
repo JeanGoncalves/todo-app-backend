deae71dca2225bf8d522e28a83d75d2029aac65d
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-06-28 11:31:28 -0300

    Include Config Vars

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo')
